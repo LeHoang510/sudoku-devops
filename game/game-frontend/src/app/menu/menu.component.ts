f3c0dc9619cb159f50a73683238051337d059412
ThuyTu <vu-thuy-tu.le@insa-rennes.fr>
2022-11-29 14:13:33 +0100

    Added navigation to board

--- game/game-frontend/src/app/menu/menu.component.ts
+++ game/game-frontend/src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../service/game.service';
-import {FormBuilder} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Level } from '../models/level';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -8,15 +10,21 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  level = Level;
+  selected = Level.easy;
   suggestion = this._formBuilder.group({
     suggested: false,
   });
-  constructor(public gameService : GameService, private _formBuilder: FormBuilder) { }
+  constructor(public gameService : GameService, private _formBuilder: FormBuilder, private router: Router,) { }
 
   ngOnInit(): void {
   }
 
   test(){
     console.log(this.gameService.game.player)
+  }
+
+  navigationBoard(){
+      this.router.navigateByUrl('/board');
   }
 }
--- game/game-frontend/src/app/models/level.ts
+++ game/game-frontend/src/app/models/level.ts
@@ -1,2 +1,9 @@
+
 export enum Level {
+    easy = "easy",
+    medium = "medium",
+    hard = "hard",
+    veryhard = "very-hard",
+    insane = "insane",
+    inhuman = "inhuman",
 }
