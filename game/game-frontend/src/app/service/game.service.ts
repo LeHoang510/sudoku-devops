7c7204363939523e803485ca653eb7469006ad25
ThuyTu <vu-thuy-tu.le@insa-rennes.fr>
2022-12-26 01:22:18 +0100

    call generated map from be

--- game/game-frontend/src/app/service/game.service.ts
+++ game/game-frontend/src/app/service/game.service.ts
@@ -47,6 +47,20 @@ export class GameService {
   
   // Try promise and async
   async initGame(){
+    // const res = await this.http.get(`http://localhost:4445/newGame/${this.level}`,{'responseType': 'json'}).toPromise()
+    // let body = JSON.parse(JSON.stringify(res))
+    // console.log('body '+body['map'])
+    // const data = body['map']
+    // for (let i = 0; i < 81; i++){
+    //   this.m.cas[i]=parseInt(data.charAt(i));
+
+    //   if (this.m.cas[i] == 0){
+    //     this.m2.cas[i] = 0;
+    //   }else{
+    //     this.m2.cas[i] = 1;
+    //   }
+    // }
+    // this.game.updateHelpTiles()
     const res = await this.http.get(`https://sudoku.diverse-team.fr/sudoku-provider/${this.level}`,{'responseType': 'text'}).toPromise()
     const data = res!
     for (let i = 0; i < 81; i++){
@@ -60,4 +74,6 @@ export class GameService {
     }
     this.game.updateHelpTiles()
   }
+
+
 }
