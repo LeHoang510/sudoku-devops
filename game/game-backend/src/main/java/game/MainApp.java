2235eb8a01a87fe3e21b394db97c2a70d3720576
ThuyTu <vu-thuy-tu.le@insa-rennes.fr>
2023-10-23 10:08:31 +0200

    test pipeline backend

--- game/game-backend/src/main/java/game/MainApp.java
+++ game/game-backend/src/main/java/game/MainApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApp { // run on port 4445 (it is defined on resources/application.properties)
 	public static void main(final String[] args) {
+		System.out.println("Game backend is running...");
 		SpringApplication.run(MainApp.class, args);
 	}
 }
