49a50c43c3538e4cb55d1ff3f64ecc4c3e1f7a22
le-hoang.nguyen <lehoangnguyen510@gmail.com>
2024-01-24 15:19:05 +0100

    fix: fix cypress

--- game/cypress/cypress/e2e/play.cy.ts
+++ game/cypress/cypress/e2e/play.cy.ts
@@ -22,30 +22,20 @@ describe('Test play game', () => {
     cy.contains('Player name');
     cy.contains('Generate random board');
 
-    cy.get('div.mat-form-field-infix.ng-tns-c49-0').type('foooooooooooooo')
+    cy.get('.mat-form-field-type-mat-input > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').type('foooooooooooooo')
 
-    cy.contains('easy').click();
+    for(let i = 0; i < 6; i++){
+      cy.get('.box-map > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').click();
+      cy.get('#mat-option-'+i).click();
+    }
+    cy.get('.box-map > .mat-form-field > .mat-form-field-wrapper > .mat-form-field-flex > .mat-form-field-infix').click();
+    cy.get('#mat-option-0').click();
 
-
-
-
-    cy.contains('hard').click();
-    cy.contains('hard').click();
-    cy.contains('inhuman').click();
-    cy.contains('inhuman').click();
-    cy.contains('insane').click();
-    cy.contains('insane').click();
-    cy.contains('medium').click();
-    cy.contains('medium').click();
-    cy.contains('very-hard').click();
-    cy.contains('very-hard').click();
-    cy.contains('easy').click();
-
-    cy.get('span.mat-checkbox-inner-container').click();
+    cy.get('.mat-checkbox-inner-container').click();
 
     cy.contains('Click on a Sudoku board to launch the game with.');
 
-    cy.get('div.board').click();
+    cy.get('.board').click();
 
     ///////////////////////////////////////////////////////////
 
@@ -54,7 +44,7 @@ describe('Test play game', () => {
     cy.contains('HISTORY');
     cy.contains('undo');
     cy.contains('redo');
-    cy.get('button.buttonRanking').click();
+    cy.get('.buttonRanking').click();
     cy.contains('Top 5 Scores');
     cy.contains('Rank');
     cy.contains('Name');
@@ -76,18 +66,18 @@ describe('Test play game', () => {
 
     cy.get('button.button-undo-redo').contains('undo').click();
     cy.get('button.button-undo-redo').contains('redo').click();
-    cy.get('div.tree-node-history.ng-star-inserted').first().click();
+    cy.get('[ng-reflect-ng-class=""] > div > img').click();
 
     const input: string = "000342850000079420284061739973186540000234978802957613000790284398425167427618395";
     const arrayOfInput: string[] = Array.from(input);
     const res: string = "719342856536879421284561739973186542165234978842957613651793284398425167427618395";
     const arrayOfRes: string[] = Array.from(res);
 
-    let count = 0;
+    let count = 1;
 
     for(let i = 0; i < arrayOfInput.length; i++){
       if(parseInt(arrayOfInput[i]) === 0){
-        let box= "div.mat-select-value.ng-tns-c84-" + (4+count);
+        let box='#mat-select-'+count*2+' > .mat-select-trigger';
         cy.get(box).click();
         cy.get("span.mat-option-text").contains(arrayOfRes[i]).click();
         count++;
